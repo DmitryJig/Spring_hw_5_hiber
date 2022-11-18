4fd15616b864a8baa04f4d00f2b980e510cad728
dima <jd2344430@gmail.com>
2022-11-18 19:46:35 +0500

    master_commit

---
+++ src/main/java/com/example/hw5_hiber1_h2/Hw5Hiber1H2Application.java
@@ -0,0 +1,13 @@
+package com.example.hw5_hiber1_h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw5Hiber1H2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw5Hiber1H2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hw5_hiber1_h2/Hw5Hiber1H2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw5_hiber1_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw5Hiber1H2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
